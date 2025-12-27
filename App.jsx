abb6533eb1d5fe5d4c015206593eba786b481ded
macfluffy <shadowimitation@hotmail.com>
2025-12-27 11:27:31 +1100

    feat: Added the initial Vite workspace to create the basic react application.
    feat: Modified the App.css and App.jsx to our templated colour palette

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+// Plugin the pages
+import HomePage from './src/pages/HomePage'
+
+function App() {
+  return (
+    <>
+      <main>
+        <HomePage />
+      </main>
+    </>
+  )
+}
+
+export default App;
