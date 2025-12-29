0a13a2574c0b95ba134afb44290e9463ce199c95
macfluffy <shadowimitation@hotmail.com>
2025-12-29 14:11:04 +1100

    feat! : Cleaning up files and adopting styled-components for a uniform solution.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,21 @@
+// Import packages
 import { useState } from "react";
+import styled from "styled-components";
 
-import "./SearchBar.css";
+// Styled components
+// Give the search bar a flat transparent rounded look
+const StyledSearchBar = styled.input`
+    /* Flat colour with transparency */    
+    background-color: #5a5b5f;
+    opacity: 0.6; 
+    
+    /* Round the corners */
+    border-radius: 10px; /* More pixel value for more roundness */
+    border: 1px solid #ccc; /* Optional: Add a border for visibility */
+    padding: 5px; /* Optional: Add padding so text doesn't touch the edges */
+
+    width: 80rem;
+`;
 
 // Search bar handles the processing of any queries submitted anywhere the component exists
 function SearchBar() {
@@ -22,7 +37,7 @@ function SearchBar() {
     return (
         <form onSubmit = {handleSubmit}>
             <label>
-                <input 
+                <StyledSearchBar 
                     type = "text"
                     className = "search-bar"
                     value = {searchTerm} 
