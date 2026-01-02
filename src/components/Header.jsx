8350301c9f2fab877deab20870c5f0a3889a5c31
macfluffy <shadowimitation@hotmail.com>
2026-01-03 09:30:54 +1100

    feat: Added the Header component, which incorporates the site logo and search bar.

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+// Import packages
+
+// Import other components
+import SearchBar from "./SearchBar";
+
+// Import image assets
+import logo from "../assets/cine_critic_logo_small.png";
+import styled from "styled-components";
+
+// Styled parts
+// Create a responsive header that doesn't wrap and maintains its shape
+const StyledHeader = styled.header`
+    display: flex;
+    flex: 1;
+`;
+
+// Small icon that will grow and shrink with the rest of the header
+const StyledLogo = styled.img`
+    display: flex;
+    flex: 0;
+    justify-content: flex-start;
+`;
+
+function Header() {
+    return (
+        <StyledHeader>
+            <StyledLogo 
+                src = {logo} 
+                alt = "CineCritic Logo"
+            />
+            <SearchBar />
+        </StyledHeader>
+    );
+}
+
+export default Header;
