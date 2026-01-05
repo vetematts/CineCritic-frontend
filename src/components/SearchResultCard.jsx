993e24d7879ba40c03339ff3dc5aa29e628be4ed
macfluffy <shadowimitation@hotmail.com>
2026-01-05 21:39:02 +1100

    feat: Added the Search Results card component. Images cannot be shown dynamically yet

---
+++ src/components/SearchResultCard.jsx
@@ -0,0 +1,46 @@
+// Import packages
+import { NavLink } from "react-router";
+import styled from "styled-components";
+
+// Make the card flexible, able to grow and shrink 
+// depending on the resolution size. Place the film 
+// poster and the description block side-by-side
+const StyledFilmCard = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+`;
+
+const StyledFilmCardContents = styled.div`
+    // Allow the columns to shrink and grow depending 
+    // on the screen size
+    flex: 1;
+`;
+
+// These are div blocks that show the films poster 
+// and details when they match a search query
+function SearchResultCard({title, releaseYear, description}) {
+    const filmPage = `/film/${title}`;
+
+    return (
+        // The whole card is clickable
+        <NavLink to = {filmPage}>
+            <StyledFilmCard id = "flex-card">
+                {/* Separate the image and the description 
+                    body into two columns */}
+                <StyledFilmCardContents id = "mini-movie-poster">
+                    <img src = {{}} />
+                </StyledFilmCardContents>
+                {/* Show maximum 3 lines, gradient into full 
+                    transparency */}
+                <StyledFilmCardContents id = "film-description">
+                    <h2>{title}</h2>
+                    <h3>({releaseYear})</h3>
+                    <p>{description}</p>
+                </StyledFilmCardContents>
+            </StyledFilmCard>
+        </NavLink>
+    );
+}
+
+export default SearchResultCard;
