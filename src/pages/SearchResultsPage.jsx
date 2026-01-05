b34ef2bcbb947527b71690e581f84462b05ec101
macfluffy <shadowimitation@hotmail.com>
2026-01-05 21:38:30 +1100

    feat: Added the skeleton to the Search Results page

---
+++ src/pages/SearchResultsPage.jsx
@@ -0,0 +1,15 @@
+import SearchResultCard from "../components/SearchResultCard";
+
+function SearchResults() {
+    return (
+        // Show the query
+        // Show the number of results matching
+        // if there was nothing
+            // Show an error image 
+        // else
+            // Show the film cards a maximum of 25 per page
+            <SearchResultCard />
+    );
+}
+
+export default SearchResults;
