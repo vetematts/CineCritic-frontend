1237f09e5cede9230bd3a958f89a02cb30a68791
vetematts <165625991+vetematts@users.noreply.github.com>
2026-01-12 18:48:28 +1000

    chore: add eslint-config-prettier and eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { FlatCompat } from '@eslint/eslintrc';
 import { defineConfig, globalIgnores } from 'eslint/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 export default defineConfig([
   globalIgnores(['dist']),
+  ...compat.extends('google', 'plugin:prettier/recommended'),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
