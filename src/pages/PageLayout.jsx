b3824cdd5a8cb36e92e4443780fdb7ed62fcf202
macfluffy <shadowimitation@hotmail.com>
2026-01-12 20:51:25 +1100

    feat: Set the width to <main> for flex items to reference when setting dimensions.

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -4,6 +4,14 @@ import { Outlet } from "react-router-dom"
 // Plugin the top level components that will exist on all the pages
 import Header from '../components/Header';
 import Footer from "../components/Footer";
+import styled from "styled-components";
+
+// Set the width of the main to match the root div container.
+// This is used for the flex container and its children to reference
+// when flexing.
+const StyledMain = styled.main`
+    width: 100%;
+`;
 
 // Create a templated layout for the webpage
 function PageLayout({currentPage}) {
@@ -12,9 +20,9 @@ function PageLayout({currentPage}) {
             {/* Only show the header when we're 
                 not on the main/front page */}
             {(currentPage !== "/") && <Header />}
-            <main>
+            <StyledMain>
                 <Outlet />
-            </main>
+            </StyledMain>
             <Footer />
         </>
     )
