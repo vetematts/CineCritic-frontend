75eba84d2769733523593fd22ca676f3e3692065
macfluffy <shadowimitation@hotmail.com>
2026-01-12 21:52:35 +1100

    feat: Advanced Search Page now fully flexes without affecting the main page

--- src/pages/AdvancedSearchPage.jsx
+++ src/pages/AdvancedSearchPage.jsx
@@ -3,68 +3,99 @@ import { useState } from "react";
 import styled from "styled-components";
 
 // Styled components
+// This is the flex container for all the inputs
+const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    // Set the form to be 100% of the main's width
+    width: 100%;
+`;
+
+const StyledSearchRows = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    
+    // Allow the columns to shrink and grow depending 
+    // on the screen size
+    flex: 1;
+
+    // Space the columns so they are now 1 column per 
+    // row in mobile resolution
+    width: 80%;
+    margin: 1rem;
+`;
+
 // Give all the labels the same gold colouring as headings
 const StyledLabels = styled.label`
-    /* Same golden font as the headings */
+    // Same golden font as the headings
     color: #e9da57;
     
-    /* Response design */
-    display: flex;
-    justify-content: flex-start;
+    // Response design
+    align-item: flex-start;
 
-    /* Spacing between itself and its input */
+    // Spacing between itself and its input
     padding: 2px;
 `;
 
 // White background input fields with slight rounded corners
 // with spacing to break up and declutter the page
 const StyledInputs = styled.input`
-    /* White background */
+    // White background
     background-color: #ffffffff;
     opacity: 0.9;
 
-    /* Round the corners */
-    border-radius: 10px; /* More pixel value for more roundness */
-    padding: 5px; /* Optional: Add padding so text doesn't touch the edges */
+    // Round the corners
+    border-radius: 10px; // More pixel value for more roundness
+    padding: 5px; // Optional: Add padding so text doesn't touch the edges
 
-    /* Set responsive design */
+    // Set responsive design
     display: flex;
-    flex: 1; /* Shrink and grow in proportion to window size */
-    justify-content: flex-start;
-    flex-basis: 60rem; /* Ideal width if space permits */
-    width: 60rem; /* This is the ideal width */
+    flex: 1; // Shrink and grow in proportion to window size
+    flex-basis: 100%; // Input will take up an entire row
 
-    /* Space away from other items */
+    // Space away from other items
     margin: 5px 0 20px 0;
 `;
 
 // Flexible drop down that fits in next to its input field
 const StyledDropDown = styled.select`
-    /* Make the drop down responsive */    
+    // Make the drop down responsive
     display: flex;
-    flex: 0;
+    flex: 1;
     justify-content: flex-start;
     align-items: flex-start;
-    height: auto;
 
-    /* Space away from other items */
+    // Space away from other items
     margin: 5px 5px 20px 0;
 `;
 
 // Create a div container for the ratings drop down and input
 const StyledRatingInput = styled.div`
-    /* Set responsive design */
+    // Set responsive design
     display: flex;
     justify-content: flex-start;
-    width: 61rem;
+    flex: 1;
+    flex-basis: 100%;
+    flex-wrap: nowrap;
+    
+    // This container is 80% of the main's width
+    width: 80%
 `;
 
 // Space the submit button further away from the last query input
 const StyledSubmitButton = styled.button`
-    /* Roughly 50px distance between this and the Genres input field */
+    // Make the button appear left for left reading
+    align-items: flex-start;    
+    
+    // Roughly 50px distance between this and the Genres input field
     margin: 4rem 0 0 0;
 
-    /* Give the button a bit more meat */
+    // Give the button a bit more meat
     height: 30px;
 `;
 
@@ -72,7 +103,7 @@ function AdvancedSearchPage() {
     const [title, setTitle] = useState("");
     const [releaseYear, setReleaseYear] = useState("");
     const [crew, setCrew] = useState("");
-    const [ratingComparator, setRatingComparator] = useState("EQUAL_TO");
+    // const [ratingComparator, setRatingComparator] = useState("EQUAL_TO");
     const [rating, setRating] = useState("");
     const [genres, setGenres] = useState("");
 
@@ -106,66 +137,77 @@ function AdvancedSearchPage() {
     };
 
     return (
-        <form>
-            <StyledLabels>
-                Movie Title
-            </StyledLabels>
-            <StyledInputs 
-                value = {title} 
-                onChange = {handleTitle}
-                placeholder = "Any word in the name of the movie"
-            />
-            <StyledLabels>
-                Release Year
-            </StyledLabels>
-            <StyledInputs 
-                value = {releaseYear} 
-                onChange = {handleReleaseYear}
-                placeholder = "Any number when the movie was released"
-            />
-            <StyledLabels>
-                Film Crew
-            </StyledLabels>
-            <StyledInputs 
-                value = {crew} 
-                onChange = {handleFilmCrew}
-                placeholder = "Any word in the name of any film crew members"
-            />
-            <StyledLabels>
-                Rating
-            </StyledLabels>
-            <StyledRatingInput>
-                <StyledDropDown 
-                    defaultValue = {"EQUAL_TO"}
-                >
-                    {/* HTML entity codes: &lt is < and &gt is > */}
-                    <option value = "LESS_THAN">Less than</option> 
-                    <option value = "LESS_OR_EQUAL">Less or equal to</option>
-                    <option value = "EQUAL_TO">Equal to</option>
-                    <option value = "GREATER_THAN">Greater than</option>
-                    <option value = "GREATER_OR_EQUAL">Greater or equal to</option>
-                </StyledDropDown>
+        <StyledForm>
+            <StyledSearchRows>
+                <StyledLabels>
+                    Movie Title
+                </StyledLabels>
+                <div className = "break"></div>
                 <StyledInputs 
-                    value = {rating} 
-                    onChange = {handleRating}
-                    placeholder = "Any number between 0 and 5"
+                    value = {title} 
+                    onChange = {handleTitle}
+                    placeholder = "Any word in the name of the movie"
                 />
-            </StyledRatingInput>
-            <StyledLabels>
-                Genres
-            </StyledLabels>
-            <StyledInputs 
-                value = {genres} 
-                onChange ={handleGenres}
-                placeholder = "Enter any genre"
-            />
+            </StyledSearchRows>
+            <StyledSearchRows>
+                <StyledLabels>
+                    Release Year
+                </StyledLabels>
+                <StyledInputs 
+                    value = {releaseYear} 
+                    onChange = {handleReleaseYear}
+                    placeholder = "Any number when the movie was released"
+                />
+            </StyledSearchRows>
+            <StyledSearchRows>
+                <StyledLabels>
+                    Film Crew
+                </StyledLabels>
+                <StyledInputs 
+                    value = {crew} 
+                    onChange = {handleFilmCrew}
+                    placeholder = "Any word in the name of any film crew members"
+                />
+            </StyledSearchRows>
+            <StyledSearchRows>
+                <StyledLabels>
+                    Rating
+                </StyledLabels>
+                <StyledRatingInput>
+                    <StyledDropDown 
+                        defaultValue = {"EQUAL_TO"}
+                    >
+                        {/* HTML entity codes: &lt is < and &gt is > */}
+                        <option value = "LESS_THAN">Less than</option> 
+                        <option value = "LESS_OR_EQUAL">Less or equal to</option>
+                        <option value = "EQUAL_TO">Equal to</option>
+                        <option value = "GREATER_THAN">Greater than</option>
+                        <option value = "GREATER_OR_EQUAL">Greater or equal to</option>
+                    </StyledDropDown>
+                    <StyledInputs 
+                        value = {rating} 
+                        onChange = {handleRating}
+                        placeholder = "Any number between 0 and 5"
+                    />
+                </StyledRatingInput>
+            </StyledSearchRows>
+            <StyledSearchRows>
+                <StyledLabels>
+                    Genres
+                </StyledLabels>
+                <StyledInputs 
+                    value = {genres} 
+                    onChange ={handleGenres}
+                    placeholder = "Enter any genre"
+                />
+            </StyledSearchRows>
             <StyledSubmitButton 
                 type = "submit"
                 onSubmit = {handleSubmitSearch}
             >
                 Search with these options
             </StyledSubmitButton>
-        </form>
+        </StyledForm>
     );
 }
 
--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -10,7 +10,10 @@ import styled from "styled-components";
 // This is used for the flex container and its children to reference
 // when flexing.
 const StyledMain = styled.main`
+    // Set this to be a container and use 100% of the root's width
     width: 100%;
+    display: flex;    
+    justify-content: center;    // All items justified center
 `;
 
 // Create a templated layout for the webpage
