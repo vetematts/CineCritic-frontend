b1f92e771a33f13b513e92503e0fb811b6ba865b
macfluffy <shadowimitation@hotmail.com>
2026-01-21 20:03:10 +1100

    feat: Taken the mapping function from the movie details page in preparation of turning it into panel components.

---
+++ src/components/MovieReviewPanel.jsx
@@ -0,0 +1,16 @@
+// This is all the reviews for this movie, shown on the movie's page
+function MovieReviewPanel({reviewsArray}) {
+    return (
+        // {reviews.map((review) => (
+        //       <li key={review.id || review._id}>
+        //         <p>{review.body || review.content || review.text}</p>
+        //         {review.rating && <StyledMeta>Rating {review.rating}</StyledMeta>}
+        //         {user?.id &&
+        //           (review.user_id === user.id ||
+        //             review.userId === user.id ||
+        //             review.user?.id === user.id) &&
+        //           editingReviewId !== (review.id || review._id) && (
+    );
+}
+
+export default MovieReviewPanel;
