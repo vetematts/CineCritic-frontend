a4f1c3fded660a0ce84845fb3c6d26d664b88a92
vetematts <165625991+vetematts@users.noreply.github.com>
2026-01-28 07:56:39 +1000

    feat: update UserProfilePage and WatchlistPage to support dynamic routing for favourites and watchlist

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -38,6 +38,8 @@ function AppRoutes() {
         <Route path="/user" element={<UserProfilePage />} />
         <Route path="/user/:id" element={<UserProfilePage />} />
         <Route path="/reviews" element={<UserReviewsPage />} />
+        <Route path="/user/:id/favourites" element={<FavouritesPage />} />
+        <Route path="/user/:id/watchlist" element={<WatchlistPage />} />
       </Route>
     </Routes>
   );
