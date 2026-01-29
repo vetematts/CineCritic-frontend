f98fd1b13c9d0f503f7c7304a54930fd90968608
macfluffy <shadowimitation@hotmail.com>
2026-01-30 00:39:47 +1100

    feat ! : Separated the CSS styling from the login page and placed both of its files into its own subfolder.

--- src/pages/loginPage/style.js
+++ src/pages/loginPage/style.js
@@ -1,12 +1,8 @@
-import { NavLink } from 'react-router-dom';
-import { LoginForm } from '../components/loginForm';
+// Import packages that allow for CSS styling to be applied to React elements
 import styled from 'styled-components';
 
-// Import image assets
-import banner from '../assets/cine_critic_logo.png';
-
 // Create the flex container for the login page
-const StyledFlexContainer = styled.div`
+export const StyledFlexContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -20,7 +16,7 @@ const StyledFlexContainer = styled.div`
 `;
 
 // Align the home logo container to centre the image
-const StyledFigure = styled.figure`
+export const StyledFigure = styled.figure`
   // Figure is the flex container for the image
   display: flex;
   flex: 1;
@@ -29,7 +25,7 @@ const StyledFigure = styled.figure`
 `;
 
 // Re-size the logo to be roughly 1/3rd to 1/4th of the screen
-const StyledLoginLogo = styled.img`
+export const StyledLoginLogo = styled.img`
   // This takes shape according to the parent, the figure
   // container
   flex: 1;
@@ -42,24 +38,10 @@ const StyledLoginLogo = styled.img`
 `;
 
 // Give the message a grayish colour similar to the footer
-const StyledMessage = styled.p`
+export const StyledMessage = styled.p`
   color: #9b9393ff;
   font-size: 0.8rem;
 
   // Provide some space between this text and the login button
   margin: 1.5rem 0 0 0;
 `;
-
-export default function LoginPage() {
-  return (
-    <StyledFlexContainer className="flex-container">
-      <StyledFigure id="banner-container">
-        <StyledLoginLogo src={banner} className="home_logo" alt="CineCritic Banner" />
-      </StyledFigure>
-      <LoginForm />
-      <StyledMessage>
-        Don&apos;t have an account? <NavLink to="/signup">Create one</NavLink>
-      </StyledMessage>
-    </StyledFlexContainer>
-  );
-}
