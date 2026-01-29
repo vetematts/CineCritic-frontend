8a2503a6f016a82cde2a0d96a0c966540fd95de3
macfluffy <shadowimitation@hotmail.com>
2026-01-29 13:34:24 +1100

    feat ! : Home Page now imports the SearchBar component by its name

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 // Import the Search Bar component
-import SearchBar from '../components/SearchBar';
+import { SearchBar } from '../components/searchbar';
 import MovieSection from '../components/MovieSection';
 import { get } from '../api/api';
 import { useAuth } from '../contexts/AuthContext';
