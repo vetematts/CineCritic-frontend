bb45c2593b444133ed22afc37861bb6672acbe7a
macfluffy <shadowimitation@hotmail.com>
2026-01-29 13:11:50 +1100

    feat ! : Page Layout now imports the header component from its new subfolder

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -2,7 +2,7 @@
 import { Outlet } from 'react-router-dom';
 
 // Plugin the top level components that will exist on all the pages
-import Header from '../components/Header';
+import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 import styled from 'styled-components';
 
