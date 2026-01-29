b922e827ec14c2e1d311f9bc0408e0ed96d7ec6a
macfluffy <shadowimitation@hotmail.com>
2026-01-30 01:17:07 +1100

    feat ! : Separated the css styling of the sign up page and placed in their own subfolder

---
+++ src/pages/signupPage/index.jsx
@@ -0,0 +1,27 @@
+// Import form component
+import { SignupForm } from '../../components/signupForm';
+
+// Import image assets
+import banner from '../../assets/cine_critic_logo.png';
+
+// Import CSS styling for the sign up page
+import { 
+  StyledFlexContainer, 
+  StyledFigure, 
+  StyledSignupLogo
+} from './style';
+
+export function SignupPage() {
+  return (
+    <StyledFlexContainer>
+      <StyledFigure id="banner-container">
+        <StyledSignupLogo 
+          src={banner} 
+          className="home_logo" 
+          alt="CineCritic Banner" 
+        />
+      </StyledFigure>
+      <SignupForm />
+    </StyledFlexContainer>
+  );
+}
